285ec5e19d7fb62fddb273b32a811ec22d6ee3bf
GabrielArrudaDev <93550330+GabrielArrudaDev@users.noreply.github.com>
2025-02-04 15:14:25 -0300

    Exercicios - Funçoes

---
+++ Aulas/Aulas - JS/Aula_3_Funcoes/scripts/exercicios.js
@@ -0,0 +1,73 @@
+// const saudacao = (nome) => {
+//   console.log(`Olá, ${nome}! Seja bem vindo!`);
+// };
+
+// saudacao('Gabriel');
+
+//
+// const a = 5;
+// const b = 10;
+
+// const soma = (n1, n2) => {
+//   return n1 + n2;
+// };
+
+// const resultado = soma(a, b);
+// console.log(resultado);
+
+//
+
+// const parOuImpar = (numb1) => {
+//   if (numb1 % 2 === 0) {
+//     console.log('Par');
+//     return;
+//   }
+//   console.log('Impar');
+// };
+
+// parOuImpar(6);
+
+//
+
+// const a = 5;
+// const b = 2;
+
+// const multiplicar = (n1, n2) => {
+//   if (n2 == false) {
+//     return n1 * 1;
+//   }
+//   return n1 * n2;
+// };
+
+// const resultado = multiplicar(a, b);
+// console.log(resultado);
+
+//
+
+// const verificarIdade = (idade) => {
+//   if (idade >= 18) {
+//     console.log('Maior idade');
+//     return;
+//   }
+//   console.log('Menor idade');
+// };
+
+// verificarIdade(18);
+
+const celsiusParaFahrenheit = (celsius) => {
+  return celsius * (9 / 5) + 32;
+};
+
+const resultado = celsiusParaFahrenheit(5);
+console.log(resultado);
+
+// const tabuada = (numero) => {
+//   let resultado = '';
+//   for (let n = 1; n <= 10; n++) {
+//     resultado += `${numero} x ${n} = ${numero * n}\n`;
+//   }
+//   return resultado;
+// };
+
+// let resultadoTabuada = tabuada(5);
+// console.log(resultadoTabuada);
